c4d70271aea5769c44e1f4c549ae7eb633e91798
Hayley Johnsey <42046097+SpaceNerd2015@users.noreply.github.com>
2019-01-06 14:40:13 -0600

    design.c
    This program was written for CS 102: Intro to Computer Programming in C. It is a print design of a corgi.

---
+++ design.c
@@ -0,0 +1,33 @@
+/*Name: Hayley Andrews
+Class: CS 102-01
+Date: 9-5-16
+Programming Assignment 1: Complex Design (Welsh Corgi)*/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("                             ___               __\n");
+	printf("                            (  \\\\             // )\n");
+	printf("                            |   \\\\           //  |\n");
+	printf("                            \\    \\\\         //   /\n");
+	printf("                             (   /~~~~~~~~~~~\\  )\n");
+	printf("                              \\ /  _     _    \\)\n");
+	printf("                               /  (_)   (_)    \\\n");
+	printf("                              /       _         \\\n");
+	printf("       ______________________(    )  (_)  /)     )\n");
+	printf("      /                       \\   (_______)/    /\n");
+	printf("     /                         \\    | | |/     /| \n");
+	printf("    /                           \\   | | |     (/\n");
+	printf("   (                  (         )   \\_|_/     /\n ");
+	printf("  |                  (                      /\n");
+	printf("   |                  )                     /|\n");
+	printf("   (      /___________(      |             / |\n");
+	printf("   |      |   /     /  \\     |\\___________/  )\n");
+	printf("   )      |   \\     (   |    |         )     |\n");
+	printf("   (      \\   |      \\  /    )         (     \\\n");
+	printf("    (_)_)_)    (_)_)_) /     |         (_)_)_)\n");
+	printf("                       (_(_(_)\n");
+
+	system("pause");
+}
